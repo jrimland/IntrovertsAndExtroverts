1129cb30b14ef56e036da96c4590a21f71f93501
Graham <Graham@104.39.87.188>
2016-03-23 15:25:21 -0400

    Created a greeting for the extrovert class

--- src/entities/Extrovert.java
+++ src/entities/Extrovert.java
@@ -5,5 +5,8 @@ package entities;
  * @author jrimland
  */
 public class Extrovert extends Customer{
-    
+    public void greeting(){
+        System.out.println("HI, I'M AN EXTROVERT!!!");
+        
+    }
 }
